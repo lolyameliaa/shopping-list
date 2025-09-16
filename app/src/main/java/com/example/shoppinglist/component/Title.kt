dc2277befacdfaa4d2008eaeb2e2e95041e226cd
lolyameliaa <lolyamelia@gmail.com>
2025-09-16 11:52:17 +0700

    first commit

---
+++ app/src/main/java/com/example/shoppinglist/component/Title.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Title() {
+    Text(
+        text = "Shopping List",
+        fontSize = 24.sp
+    )
+}
